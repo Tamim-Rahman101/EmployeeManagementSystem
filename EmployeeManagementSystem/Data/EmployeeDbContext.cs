32db651d7d23a077cb1eebbc6c8d9e1e39744b5d
tamim <tamim101rahman@gmail.com>
2026-05-11 18:39:58 +0600

    implemented dbcontext

---
+++ EmployeeManagementSystem/Data/EmployeeDbContext.cs
@@ -0,0 +1,19 @@
+﻿using EmployeeManagementSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagementSystem.Data
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<EmployeeType> EmployeeTypes { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Designation> Designations { get; set; }
+
+
+    }
+}
